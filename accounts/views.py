e9b8a468788dd2d6a6093014171f792212598900
ken <ken.shirou12341234@gmail.com>
2026-09-25 21:27:10 +0900

    サーバーエラー解消

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
-from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -13,12 +12,15 @@ class RegisterView(CreateView):
     success_url = reverse_lazy("top:index")
 
     def form_valid(self, form):
-        # ユーザーを作成・保存
-        user = form.save()
-        # 新規登録直後にそのまま自動ログインを実行
-        login(self.request, user)
-        # success_urlへリダイレクト
-        return HttpResponseRedirect(self.get_success_url())
+        # 1. super().form_valid(form) を呼ぶことで self.object (作成されたユーザー) が自動でセットされ、
+        #    リダイレクト用レスポンスが生成されます
+        response = super().form_valid(form)
+        
+        # 2. 作成されたユーザーを取得してログインを実行
+        login(self.request, self.object)
+        
+        # 3. 成功レスポンスを返す
+        return response
 
 
 class CustomLoginView(LoginView):
